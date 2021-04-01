b4dcea9308dcc842ab060fc8768e4a6895f04336
goniti <goniti@live.fr>
2021-04-01 15:01:50 +0200

    rework button play and add class for all cards

--- js/main.js
+++ js/main.js
@@ -1,19 +1,44 @@
 const game = {
 	bankroll: 500,
-	element: () => {
-		game.bankAmount = document.querySelector("#bankroll");
+	elements: () => {
+		bankAmount = document.querySelector('#bankroll')
+		buttonPlay = document.querySelector('#bet-play')
+		buttonPlayAgain = document.querySelector('#bet-again')
+		formBet = document.querySelector('#bet-form')
+		cardLeft = document.querySelector('.board__card--left')
+		cardRight = document.querySelector('.board__card--right')
+		cardMiddle = document.querySelector('.board__card--middle')
+	},
+	distributionOfCard: () => {
+		cardLeft.className === ' board__card--${min}'
+		cardRight.className === ' board__card--${max}'
+	},
+	ShowHideButtons: () => {
+		buttonPlay.addEventListener('click', () => {
+			buttonPlay.style.display = 'none'
+			formBet.style.display = 'block'
+		})
+		buttonPlayAgain.addEventListener('click', () => {
+			buttonPlayAgain.style.display = 'none'
+			formBet.style.display = 'block'
+		})
+		formBet.addEventListener('submit', (event) => {
+			formBet.style.display = 'none'
+			buttonPlayAgain.style.display = 'block'
+			event.preventDefault()
+		})
 	},
 	showBankRoll: () => {
-		game.bankAmount.textContent = `Bank: ${game.bankroll}`;
+		bankAmount.textContent = `Bank: ${game.bankroll}`
 	},
 	launch: () => {
-		game.showBankRoll();
+		game.ShowHideButtons()
+		game.showBankRoll()
 	},
-
 	init: () => {
-		game.element();
+		game.elements()
 		game.launch()
-	}
+	},
 }
 
-document.addEventListener('DOMContentLoaded', game.init);
+document.addEventListener('DOMContentLoaded', game.init)
